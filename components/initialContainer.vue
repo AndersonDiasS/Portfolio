607947ccdff3bc0e490028dc9a74023ce674abf5
AndersonDiasS <andersondiassiqueira33@outlook.com>
2021-08-10 11:45:30 -0300

    Content

---
+++ components/initialContainer.vue
@@ -0,0 +1,6 @@
+<template lang="pug">
+ div
+    h1 Conteudo
+</template>
+<script></script>
+<style></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <Tutorial/>
+  <initialContainer />
 </template>
 
 <script>
