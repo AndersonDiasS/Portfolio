5218a7b09a2d4c3d6bbcc86c8a842e746c491d3b
AndersonDiasS <andersondiassiqueira33@outlook.com>
2021-08-20 22:47:46 -0300

    Exclusão do GH-Pages

--- gh_pages/_nuxt/static/1629506752/state.js
+++
@@ -1,5 +0,0 @@
-<<<<<<< HEAD:gh_pages/_nuxt/static/1629506752/state.js
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FPortfolio\u002F_nuxt\u002Fstatic\u002F1629506752",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002FPortfolio\u002F",assetsPath:"\u002FPortfolio\u002F_nuxt\u002F",cdnURL:a}}}}(null));
-=======
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FPortfolio\u002F_nuxt\u002Fstatic\u002F1629506474",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002FPortfolio\u002F",assetsPath:"\u002FPortfolio\u002F_nuxt\u002F",cdnURL:a}}}}(null));
->>>>>>> 2b29f0981a5d26d9bf58801dd260d628437a058f:gh_pages/_nuxt/static/1629506474/state.js
