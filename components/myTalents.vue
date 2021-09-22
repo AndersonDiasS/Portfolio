eb9df69dc6b4504861cc38acab982f2f22bc7582
AndersonDiasS <andersondiassiqueira33@outlook.com>
2021-09-22 17:36:19 -0300

    Conclusão de Talentos

--- components/myTalents.vue
+++ components/myTalents.vue
@@ -25,10 +25,14 @@
       </g>
     </svg>
     <div class="bgPrimary" style="padding-bottom: 20px">
-      <h2 class=" text-center p-t-b">Habilidades 🧙‍♂️</h2>
+      <h2 class="text-center p-t-b">Habilidades 🧙‍♂️</h2>
 
       <div class="flexSpaceAround container">
-        <div class="card" v-for="card in cards" :key="card.titulo">
+        <div
+          class="card gradient-border"
+          v-for="card in cards"
+          :key="card.titulo"
+        >
           <ul class="text-center">
             <li><i :class="card.icone" class="cardIcon"></i></li>
           </ul>
@@ -64,7 +68,7 @@ export default {
           tecnologias: 'Javascript, PHP, SCSS, HTML',
           icone: 'fab fa-js-square',
         },
-           {
+        {
           titulo: 'PHP',
           experiencia: '3 anos',
           projetos: '8',
@@ -99,21 +103,20 @@ export default {
 </script>
 
 <style scoped>
-.bgPrimary h2{
+.bgPrimary h2 {
   margin-bottom: 50px;
 }
- 
 
-.card{
+.card {
   margin-bottom: 30px;
-  width:230px;
+  width: 230px;
 }
-.cardIcon { 
+.cardIcon {
   font-size: 60px;
 }
 .card ul {
   padding: 0;
-  margin:0;
+  margin: 0;
 }
 .card ul li {
   color: #3b0a66;
@@ -125,6 +128,34 @@ export default {
 .p-t-b {
   padding-top: 20px;
   padding-bottom: 20px;
-  
+}
+
+.gradient-border:after,
+.gradient-border:hover {
+  --border-width: 3px;
+  transition: 1s;
+  content: '';
+  background: linear-gradient(
+    60deg,
+    hsl(224, 85%, 66%),
+    hsl(269, 85%, 66%),
+    hsl(314, 85%, 66%),
+    hsl(359, 85%, 66%),
+    hsl(44, 85%, 66%),
+    hsl(89, 85%, 66%),
+    hsl(134, 85%, 66%),
+    hsl(179, 85%, 66%)
+  );
+  border-radius: var(--border-width);
+  background-size: 300% 300%;
+  background-position: 0 50%;
+  border-radius: calc(2 * var(--border-width));
+  animation: moveGradient 4s alternate infinite;
+}
+
+@keyframes moveGradient {
+  50% {
+    background-position: 100% 50%;
+  }
 }
 </style>
